e39b34673b7ac8e79e545513f8661ecc918421c2
ChanchalGMali <chanchalmali335@gmail.com>
2026-03-11 14:59:19 +0530

    Update settings.py

--- spamDetection/settings.py
+++ spamDetection/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2j6l&zz0pyu1ak$szgo1xwt!=wrvh(0qq7+*%9dynm1rp3zz$a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
