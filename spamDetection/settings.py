4ae9abcf7ae21425c8a430cd6bd7e4a0794dd909
Sanja <sanja123@gmail.com>
2026-03-24 19:56:52 +0530

    fix Procfile

--- spamDetection/settings.py
+++ spamDetection/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,11 +25,10 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 import os
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['spamDetection.onrender.com']  # or your-domain.onrender.com later
 
 # Application definition
 
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'spamDetection.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+        conn_max_age=600
+    )
 }
 
 
@@ -124,3 +126,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- spamDetection/urls.py
+++ spamDetection/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from detector import views
+from spamDetection import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
