74a80a865c187e5046c8bf62d29b3efdfe06dffb
Sanja <sanja123@gmail.com>
2026-03-26 16:50:40 +0530

    fix runtime location final

--- models.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.conf import settings
-
-model_path = os.path.join(settings.BASE_DIR, 'model.pkl')
-model = pickle.load(open(model_path, 'rb'))
-
--- sms-spam-Detection/spamApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpamappConfig(AppConfig):
-    name = 'spamApp'
